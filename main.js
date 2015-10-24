4af35d90d103df481c5950ffb820177de15b89f5
yuwen41200 <yuwen41200@gmail.com>
2015-10-25 01:12:19 +0800

    Finished UI/UX. Just kidding :)

--- main.js
+++ main.js
@@ -13,15 +13,26 @@ $.getJSON( "data.json", function(data) {
 	console.log(items);
 });
 
-var pieChart = d3.select('#piechart');
-
-pieChart
+var chart1 = d3.select('#chart1');
+chart1
 	.attr('width', 100)
 	.attr('height', 100);
-
-pieChart.append('rect')
+chart1.append('rect')
 	.attr('x', 20)
 	.attr('y', 20)
 	.attr('width', 60)
 	.attr('height', 60)
 	.attr('fill', '#232323');
+
+var chart2 = d3.select('#chart2');
+chart2
+	.attr('width', 100)
+	.attr('height', 100);
+chart2.append('rect')
+	.attr('x', 20)
+	.attr('y', 20)
+	.attr('width', 60)
+	.attr('height', 60)
+	.attr('stroke', '#ff6600')
+	.attr('stroke-width', 5.5)
+	.attr('fill-opacity', 0.1);
