fbbc390126fce18ace087554cc1dbd5cf779dcc7
yuwen41200 <yuwen41200@gmail.com>
2015-10-25 03:13:28 +0800

    yeeeeeeeee

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
-$.getJSON( "data.json", function(data) {
-	'use strict';
-	var items = [];
-	$.each(data, function(key, val) {
+var root = null;
+var items = [];
+
+$.getJSON("data.json", function(data) {
+	root = data;
+});
+
+function analyzeReceivedNode(node) {
+	var level = 1;
+	var node_tmp = node;
+	while (!node_tmp.hasOwnProperty("畢業生總計")) {
+		node_tmp = node_tmp[Object.keys(node_tmp)[0]];
+		++level;
+	}
+	switch (level) {
+		case 1: y(node); break;
+		case 2: ye(node); break;
+		case 3: yee(node); break;
+		case 4: yeee(node); break;
+		default: console.log("Invalid level value!"); break;
+	}
+}
+
+function y(node) {
+	$.each(node, function(keyyyy, vallll) {
+		items.push({foo: keyyyy, bar: vallll});
+	});
+}
+
+function ye(node) {
+	$.each(node, function(keyyy, valll) {
+		$.each(valll, function(keyyyy, vallll) {
+			items.push({foo: keyyyy, bar: vallll});
+		});
+	});
+	parseItemArray(items);
+}
+
+function yee(node) {
+	$.each(node, function(keyy, vall) {
+		$.each(vall, function(keyyy, valll) {
+			$.each(valll, function(keyyyy, vallll) {
+				items.push({foo: keyyyy, bar: vallll});
+			});
+		});
+	});
+	parseItemArray(items);
+}
+
+function yeee(node) {
+	$.each(node, function(key, val) {
 		$.each(val, function(keyy, vall) {
 			$.each(vall, function(keyyy, valll) {
 				$.each(valll, function(keyyyy, vallll) {
@@ -10,8 +57,18 @@ $.getJSON( "data.json", function(data) {
 			});
 		});
 	});
-	console.log(items);
-});
+	parseItemArray(items);
+}
+
+function parseItemArray(items) {
+	for (var i = 0; i < 43; ++i) {
+		var j = i + 43;
+		while (j < items.length) {
+			items[i].bar += items[j].bar;
+			j += 43;
+		}
+	}
+}
 
 var chart1 = d3.select('#chart1');
 chart1
