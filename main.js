23628a4827dd5347380486840492600c9a5a2f08
yuwen41200 <yuwen41200@gmail.com>
2015-10-25 00:07:25 +0800

    Testing index.

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
+$.getJSON( "data.json", function(data) {
+	'use strict';
+	var items = [];
+	$.each(data, function(key, val) {
+		$.each(val, function(keyy, vall) {
+			$.each(vall, function(keyyy, valll) {
+				items.push({foo: keyyy, bar: valll});
+			});
+		});
+	});
+	console.log(items);
+});
+
 var pieChart = d3.select('#piechart');
-var bubbleChart = d3.select('#bubblechart');
 
 pieChart
-	.attr('width', 800)
-	.attr('height', 800);
-
-bubbleChart
-	.attr('width', 800)
-	.attr('height', 800);
+	.attr('width', 100)
+	.attr('height', 100);
 
 pieChart.append('rect')
-	.attr('x', 0)
-	.attr('y', 0)
+	.attr('x', 20)
+	.attr('y', 20)
 	.attr('width', 60)
 	.attr('height', 60)
-	.attr('fill', '#232323');
-
-
-bubbleChart.append('g')
 	.attr('fill', '#232323');
